2b052150af9e3abb05651a2168bf68be4603f408
JoseCancino <jose.cancino@tecsup.edu.pe>
2021-08-26 14:11:35 -0500

    lab01 parte 2

---
+++ lab01/prjLab01-1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjLab01_1
+{
+    class Program
+    {
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        static void Raiz()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("La raiz cuadrada de {0} es: {1}", i, Math.Sqrt(i));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Procedimientos y Funciones";
+            string opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Suma de dos numeros");
+                Console.WriteLine("[2] Imprimir raiz cuadrada de los 10 primeros numeros enteros");
+                Console.WriteLine("[0] Salir");
+                Console.WriteLine("Ingrese una opcion y presione ENTER");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el primer numero");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo numero");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Calculando...");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!opcion.Equals("0"));
+        }
+    }
+}
