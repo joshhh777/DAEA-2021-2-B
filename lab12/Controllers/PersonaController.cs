c93b90c81f685ae95e1556d558102b52b8469fd8
JoseCancino <jose.cancino@tecsup.edu.pe>
2021-11-08 11:18:46 -0500

    lab 12 terminado

---
+++ lab12/Controllers/PersonaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab12.Models;
+
+namespace lab12.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "Jose",
+                Apellido = "Cancino",
+                Direccion = "Av. Fco Bolognesi 311",
+                FechaNac = Convert.ToDateTime("2000-01-27"),
+                Email = "jcancino@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 234",
+                FechaNac = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@gmail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av. Los Manzanos 101",
+                FechaNac = Convert.ToDateTime("1982-02-14"),
+                Email = "carlos@gmail.com"
+            });
+            return View(personas);
+        }
+    }
+}
