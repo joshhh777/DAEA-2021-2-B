83932392a471bb61ee3e5ef19d3bf428e42b64d1
JoseCancino <jose.cancino@tecsup.edu.pe>
2021-11-23 23:35:55 -0500

    Lab 14 completo

---
+++ lab14/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
+
+namespace lab14.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        #region Contexto
+        private NorthwndEntities _contexto;
+
+        public NorthwndEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new NorthwndEntities();
+                return _contexto;
+            }
+        }
+        #endregion
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EditProducts(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Products ProductsEditar = Contexto.Products.Find(id);
+            if (ProductsEditar == null)
+                return HttpNotFound();
+
+            return View(ProductsEditar);
+        }
+        [HttpPost]
+        public ActionResult EditProducts(Products ProductsEditar)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Contexto.Entry(ProductsEditar).State = EntityState.Modified;
+                    Contexto.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(ProductsEditar);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
