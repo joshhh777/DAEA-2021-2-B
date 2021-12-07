3c4469f99686f15c9706fb963439e615597948c9
JoseCancino <jose.cancino@tecsup.edu.pe>
2021-12-06 19:22:58 -0500

    Lab16 completado

---
+++ lab16/Models/Person.Context.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace persons.Models
+{
+    public class PersonContext: DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options): base(options)
+        {
+
+        }
+        public DbSet<Person> Person {get; set;}
+    }
+}
